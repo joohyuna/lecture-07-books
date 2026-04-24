06a4e9dd47027a9684ef3315ef76727a4dbd3b5a
mcm <joohyuna2@gmail.com>
2026-04-24 17:54:55 +0900

    search 진행중

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,18 @@
-import styles from "./SearchBar.module.css";
-import { type SubmitEvent, type ChangeEvent, useState } from "react";
 import { useNavigate } from "react-router";
+import styles from "./SearchBar.module.css";
+import { type ChangeEvent, type SubmitEvent, useState } from "react";
 
+// 사용자에 입력을 받는것
 function SearchBar() {
     const navigate = useNavigate(); // navigate라고 하는 변수에 이동에 관련된 기능 담아줘야함
-    const [keyword, setKeyword] = useState("");  // input 입력 되는 값을
+    const [keyword, setKeyword] = useState(""); // input 입력 되는 값을
+
+    const onChange = (event: ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
+        // 사용자가 입력한 값을 받아다가, keyword라고 하는 state에 저장
+        // 사용자가 입력한 값을 => event.target.value
+        setKeyword(event.target.value);
+    };
+
     const onSubmit = (event: SubmitEvent<HTMLFormElement>) => {
         // form onSubmit라고 하는 기능은, 브라우저 축에 기본 기능이 "새로고침 하고 전달" 이 있음
         // 이걸 하지 못하도록 해야 함.
@@ -15,23 +23,23 @@ function SearchBar() {
         // string의 맨 앞과 맨 뒤에 존재할 수 있는 공백을 제거하는 메서드 : trim()
         // "" 에서 사용자가 enter -> keyword.trim() -> ""   -> if (keyword.trim()) ->  false
         // " "에서 사용자가 enter -> keyword.trim() -> ""
-        if (!keyword.trim()) return; // trim을 했더니, 값이 없으면 빈 스트림 이면 return으로 끝내라
+        // if (!keyword.trim()) return; // trim을 했더니, 값이 없으면 빈 스트림 이면 return으로 끝내라
+        const k = keyword.trim();
+        if (!k) return;
         // keyword을 활용해서 사용자를 강제 이동
         // 원래 URL은 한글이 X 않된다. 영어와 몇가지의 특수문자 만 가능
         // 해석을 할 수 있도록 영어람 몇가지의 특수문자를 이용해 변환 작업이 필요함 -> encodeURIComponent()
-        navigate(`/search?keyword=${encodeURIComponent(keyword)}`);
+        navigate(`/search?keyword=${encodeURIComponent(k)}`);
     };
 
-    const onChange = (event: ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
-        // 사용자가 입력한 값을 받아다가, keyword라고 하는 state에 저장
-        // 사용자가 입력한 값을 => event.target.value
-        setKeyword(event.target.value);
-    };
+
 
     return (
         <form className={styles.box} onSubmit={onSubmit}>
             <input className={styles.input} onChange={onChange} />
-            <button type={"submit"} className={styles.button}>검색</button>
+            <button type={"submit"} className={styles.button}>
+                검색
+            </button>
         </form>
     );
 }
--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,60 +1,74 @@
 import { useSearchParams } from "react-router";
 import { useEffect, useState } from "react";
+import styles from "./Search.module.css";
+const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
-type BookItem = {
+// JSON에는 값이 있음 때만 보여 준다. 없을 때는 없다가 아니라 아얘 보여주지 않음
+type BookType = {
     id: string;
     volumeInfo: {
         title: string;
         authors?: string[];
-        description: string;
+        description?: string;
+        publishedDate: string;
         imageLinks?: {
             thumbnail?: string;
-            small?: string;
+            smallThumbnail?: string;
         };
     };
 };
 
+type ApiResponseType ={items: BookType[]};  // 대부분 type으로 만든 초록색 대부분 객체이다.
+
 function Search() {
+    // 사용자가 요청한 키워드를 받아서, 그것을 가지고 google API요청을 하고, 받아온 결과를 화면에 출력해주는 일
     // query string으로 둘어온 값을 꺼내오기 위해서  useSearchParams를 사용
-    const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+
     // useParams를 사용할 때는 const {id}= useParams();
     // useSearchParams는 useState와 사용법이 동일
-    const [params, setParams] = useSearchParams(); // queryString내용이 params에 담겨 나옴
-    const k = params.get("keyword"); // "수학"이라는게 있을 수도 있지만,  없을 수도 있음
+    const [searchParams, setSearchParams] = useSearchParams(); // queryString내용이 params에 담겨 나옴
+
+    const keyword = searchParams.get("keyword"); // "수학"이라는게 있을 수도 있지만,  없을 수도 있음
 
     // keyword준비 됬으니, API를 통해 요청한 정보를 받아다가 화면에 출력만 해주면 됨
-    const [loading, setLoading] = useState(true);
-    const [list, setList] = useState<BookItem[]>([]);
+    //const [loading, setLoading] = useState<boolean>(true);
+    const [list, setList] = useState<BookType[]>([]);  // [] 알겠어, 근데 그안에 들어가는 요소의 타입이 뭐야?
 
     useEffect(() => {
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=${k}&maxResults=20&key=${API_KEY}`)
+        if (!keyword) {
+            //setLoading(false); // 잠깐 빨간줄 무시
+            return;
+        } // 키워드가 없다면 실행하지 말아라
+
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${keyword}&maxResults=20&key=${API_KEY}`)
             .then(res => res.json())
-            .then(json => {
+            .then((json: ApiResponseType)  => {
                 setList(json.items);
-                setLoading(false);
+                //setLoading(false);
             })
-            .catch(err => console.log(err));
-    }, [k, API_KEY]);
-    useEffect(() => {
-        console.log(list);
-    }, [list]);
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-    return (
-        <div>
-            <h1>
-                검색 결과 : <span>{k}</span>
-            </h1>
-            <div>
-                {list.map((book: BookItem) => (
-                    <div key={book.id}>
-                        <div>{book.volumeInfo.title}</div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+            .catch(err => {
+                console.log(err);
+                //setLoading(false);
+            });
+    }, [keyword]);
+
+    // 로딩에 대해서 처리를 해줘야 하나?  속도가 너무 빠르면 로딩을 넣지 않아도 되는것도 좋다
+    // 사용자에게는 로딩이 여기서는 않좋을수도 있다
+    // 로딩이 true
+    //if (loading) return <div>Loading....</div>
+
+    // 로딩이 false
+    // 값을 못 가져 왔을 때?
+
+    return<div className={styles.wrap}>
+        <h3>검색 결과 : {keyword}</h3>
+
+        {/* 검색 결과 (책 목록) 출력 */}
+        {/* 데이터가 도착했는지 안했는지, 목폭이 있는지 없는지 판단 해줘야 되나? */}
+        {list.length === 0 && (<div>검색 결과가 없습니다.</div>)}
+        {list && list.map(book => ())}
+        {/* map이가고 하는 메소드는 꼭 대상이 array여야만 쓸수 있으니, list && 로 체크를 해줄땐, list가 null이 되나?를 생각해봐야 겠지만, 여기서 모든 조건을 따져봤을 때 list는 무조건 array이긴 하지만 구지 논리곱으로 체크해줄 필요가 없다. */}
+    </div>
 }
 
 export default Search;
